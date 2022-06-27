d40d933c4a852aca94f25d3d92880496504cb2d7
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2022-06-26 22:49:19 -0300

    Button layout changed on previous diagnoses table

--- resources/views/previous-diagnoses/index.blade.php
+++ resources/views/previous-diagnoses/index.blade.php
@@ -11,34 +11,34 @@
         <table class="w-full text-sm text-gray-500 dark:text-gray-400 previous-diagnoses-table border mb-4">
             <thead class="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th class="px-6 py-4">Date of diagnosis</th>
-                    <th class="px-6 py-4"></th>
-                    <th class="px-6 py-4"></th>
+                    <th class="px-4 py-2">Date of diagnosis</th>
+                    <th class="px-4 py-2"></th>
                 </tr>
             </thead>
             <tbody>
                 @forelse($diagnoses as $diagnosis)
                     <tr class="bg-white border-b border-t dark:border-gray-700">
-                        <td class="px-6 py-4">{{$diagnosis->created_at->format('m/d/Y H:i')}}</td>
-                        <td class="px-6 py-4">
-                            @if(!$diagnosis->marked_as_correct)
-                                <form action="{{route('previous-diagnoses.mark-as-correct', [$diagnosis])}}" method="POST">
-                                    @csrf
-                                    <x-button>
-                                        Mask as correct
-                                    </x-button>
-                                </form>
-                            @else
-                                {{$diagnosis->displayWhenMarkedAsCorrect()}}
-                            @endif
-                        </td>
-                        <td class="px-6 py-4">
-                            <x-link :href="route('previous-diagnoses.show', [$diagnosis])">Check diagnosis</x-link>
+                        <td class="px-4 py-2">{{$diagnosis->created_at->format('m/d/Y H:i')}}</td>
+                        <td class="px-4 py-2">
+                            <div>
+                                @if(!$diagnosis->marked_as_correct)
+                                    <form action="{{route('previous-diagnoses.mark-as-correct', [$diagnosis])}}" method="POST">
+                                        @csrf
+                                        <x-button>
+                                            Mask as correct
+                                        </x-button>
+                                    </form>
+                                @else
+                                    {{$diagnosis->displayWhenMarkedAsCorrect()}}
+                                @endif
+                            </div>
+                        
+                            <x-link class="mt-2" :href="route('previous-diagnoses.show', [$diagnosis])">Check diagnosis</x-link>
                         </td>
                     </tr>
                 @empty
                     <tr class="bg-white border-b border-t dark:border-gray-700">
-                        <td class="px-6 py-4" colspan="4">No previous diagnoses where found</td>
+                        <td class="px-4 py-2" colspan="4">No previous diagnoses found</td>
                     </tr>
                 @endforelse
             </tbody>
