42556a6fe1af97ea5ff3bd6ba3d7482ce2d3471b
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2022-06-26 22:57:22 -0300

    Previous diagnoses layout change

--- resources/views/previous-diagnoses/index.blade.php
+++ resources/views/previous-diagnoses/index.blade.php
@@ -8,42 +8,42 @@
     <x-main-container>
         <h2 class="text-xl mb-4">History of previous diagnoses</h2>
 
-        <table class="w-full text-sm text-gray-500 dark:text-gray-400 previous-diagnoses-table border mb-4">
-            <thead class="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th class="px-4 py-2">Date of diagnosis</th>
-                    <th class="px-4 py-2"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($diagnoses as $diagnosis)
-                    <tr class="bg-white border-b border-t dark:border-gray-700">
-                        <td class="px-4 py-2">{{$diagnosis->created_at->format('m/d/Y H:i')}}</td>
-                        <td class="px-4 py-2">
-                            <div>
-                                @if(!$diagnosis->marked_as_correct)
-                                    <form action="{{route('previous-diagnoses.mark-as-correct', [$diagnosis])}}" method="POST">
-                                        @csrf
-                                        <x-button>
-                                            Mask as correct
-                                        </x-button>
-                                    </form>
-                                @else
-                                    {{$diagnosis->displayWhenMarkedAsCorrect()}}
-                                @endif
-                            </div>
+        @if(count($diagnoses))
+            <table class="w-full text-sm text-gray-500 dark:text-gray-400 previous-diagnoses-table border mb-4">
+                <thead class="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th class="px-4 py-2">Date of diagnosis</th>
+                        <th class="px-4 py-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($diagnoses as $diagnosis)
+                        <tr class="bg-white border-b border-t dark:border-gray-700">
+                            <td class="px-4 py-2">{{$diagnosis->created_at->format('m/d/Y H:i')}}</td>
+                            <td class="px-4 py-2">
+                                <div>
+                                    @if(!$diagnosis->marked_as_correct)
+                                        <form action="{{route('previous-diagnoses.mark-as-correct', [$diagnosis])}}" method="POST">
+                                            @csrf
+                                            <x-button>
+                                                Mask as correct
+                                            </x-button>
+                                        </form>
+                                    @else
+                                        {{$diagnosis->displayWhenMarkedAsCorrect()}}
+                                    @endif
+                                </div>
                         
-                            <x-link class="mt-2" :href="route('previous-diagnoses.show', [$diagnosis])">Check diagnosis</x-link>
-                        </td>
-                    </tr>
-                @empty
-                    <tr class="bg-white border-b border-t dark:border-gray-700">
-                        <td class="px-4 py-2" colspan="4">No previous diagnoses found</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                                <x-link class="mt-2" :href="route('previous-diagnoses.show', [$diagnosis])">Check diagnosis</x-link>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-        {{$diagnoses->links()}}
+            {{$diagnoses->links()}}
+        @else
+            No previous diagnoses found
+        @endif
     </x-main-container>
 </x-app-layout>
